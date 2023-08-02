8e5a45c0a1e4a5376b5f36553fa7b563f0a3840d
koehdcks <chan3_3@naver.com>
2023-08-03 01:08:15 +0900

    commit

--- study_event/eventEx1.js
+++ study_event/eventEx1.js
@@ -2,16 +2,35 @@ const cb=document.getElementsByClassName("cb");
 const btn=document.getElementById("btn");
 
 
-if(cb[0].checked){
-    for(let i=1;i<cb.length;i++){
-        cb[i].checked=true;
-    }
-}
-
-cb[0].addEventListener("change",function(){
+cb[0].addEventListener("click",function(){
     if(cb[0].checked){
         for(let i=1;i<cb.length;i++){
             cb[i].checked=true;
         }
+    }else{
+        for(let i=1;i<cb.length;i++){
+            cb[i].checked=false;
+        }
+    }
+    
+});
+
+for(let i=1;i<cb.length;i++){
+    cb[i].addEventListener("click",function(){
+        if(cb[i].checked==false){
+            cb[0].checked=false;
+        }
+        if(cb[1].checked&cb[2].checked&cb[3].checked&cb[4].checked){
+            cb[0].checked=true;
+        }
+    })
+}
+
+btn.addEventListener("click",function(){
+    if(cb[1].checked&cb[2].checked&cb[3].checked&cb[4].checked){
+        alert("회원가입");
+    }else{
+        alert("동의하세요");
     }
 });
+
