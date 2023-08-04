f3302c8f40a43d04ae67d72918101f95cdfa7fe1
koehdcks <chan3_3@naver.com>
2023-08-04 16:53:52 +0900

    0804

---
+++ study_object/object1.js
@@ -0,0 +1,15 @@
+
+//object
+let obj = {
+    num1:10,
+    num2:20,
+    method1:function(){
+        alert("method")
+    }};
+
+obj.num1=20;
+obj.num3=50;
+obj.method1();
+console.log(obj.num1);
+console.log(obj.num2);
+console.log(obj.num3);
